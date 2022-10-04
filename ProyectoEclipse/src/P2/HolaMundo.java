49a7023860941275c45cefd95ba510901a8a4b84
franciscodsa <franciscojesusdasilvaarana@gmail.com>
2022-10-04 20:09:15 +0200

    eclipse

--- ProyectoEclipse/src/P2/HolaMundo.java
+++ ProyectoEclipse/src/P2/HolaMundo.java
@@ -3,7 +3,8 @@ package P2;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.print("¡Hola Mundo!");
+		// TODO Auto-generated method stub
+		System.out.println("¡Hola Mundo!");
 
 	}
 
--- ProyectoEclipse/src/P2/Prueba.java
+++
@@ -1,9 +0,0 @@
-package P2;
-
-public class Prueba {
-	public static void main(String[] args) {
-		System.out.print("esto es una prueba");
-
-	}
-
-}
